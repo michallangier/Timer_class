3510da3ec53b081f15a396e3a9fd5d381f750875
michallangier <m.langier@wp.pl>
2022-06-04 03:08:37 +0200

    timer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = { time: {}, seconds: 0 }
+    this.startTimer = this.startTimer.bind(this)
+    this.countUp = this.countUp.bind(this)
+  }
+
+  secondsToTime(secs) {
+    let seconds = secs
+    let obj = {
+      "s": seconds
+      // ...
+    }
+    return obj
+  }
+
+  componentDidMount() {
+    let timeLV = this.secondsToTime(this.state.seconds)
+    this.setState({ time: timeLV })
+    this.startTimer()
+  }
+
+  startTimer() {
+    this.timer = setInterval(this.countUp, 1000)
+
+  }
+
+  countUp() {
+    let seconds = this.state.seconds + 1
+    this.setState({
+      time: this.secondsToTime(seconds),
+      seconds: seconds,
+    })
+  }
+
+  render() {
+
+    return (
+      <div>
+        Seconds from app start: {this.state.time.s} [s]
+      </div>
+    )
+  }
+
 }
 
 export default App;
